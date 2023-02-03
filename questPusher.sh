f92f7c95bd2d4700775a071b2afddcf3f6b9110c
rubendgpedrosa <rubenpedrosa993@gmail.com>
2023-02-03 22:47:29 +0000

    2023-02-03 22:47

--- questPusher.sh
+++ questPusher.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # add all files to git
-git add .
+/usr/bin/git add .
 
 # make commit with message as the current date in format YYYY-mm-dd
 current_date=$(date +%Y-%m-%d\ %H:%M)
-git commit -m "$current_date"
+/usr/bin/git commit -m "$current_date"
 
 # push changes to repository
-git push origin main
+/usr/bin/git push origin main
