9795d16ee06fc743e4a72ea4a1964917b56221a7
rubendgpedrosa <rubenpedrosa993@gmail.com>
2023-02-03 22:56:01 +0000

    2023-02-03 22:56

--- questPusher.sh
+++ questPusher.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd /root/PoGoLeiria
+
 # add all files to git
-/usr/bin/git add .
+/usr/bin/git add /root/PoGoLeiria/.
 
 # make commit with message as the current date in format YYYY-mm-dd
 current_date=$(date +%Y-%m-%d\ %H:%M)
