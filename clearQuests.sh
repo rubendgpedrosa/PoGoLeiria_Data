7eeca9d9ee31d7d6401918312f0523c1fd504f17
rubendgpedrosa <rubenpedrosa993@gmail.com>
2023-02-11 10:07:01 +0000

    2023-02-11 10:07

--- clearQuests.sh
+++ clearQuests.sh
@@ -3,5 +3,5 @@
 files=(/root/PoGoLeiria/battling.json /root/PoGoLeiria/others.json /root/PoGoLeiria/catching.json /root/PoGoLeiria/battling.json /root/PoGoLeiria/throwing.json)
 
 for file in "${files[@]}"; do
-  echo "[]" > "$file"
+  echo "[{}]" > "$file"
 done
